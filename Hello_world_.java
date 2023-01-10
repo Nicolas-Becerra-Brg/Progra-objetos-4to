6023f587c716b2f8a0d475ea4b1d99e75c78cb1c
Nicolas-Becerra-Brg <122307985+Nicolas-Becerra-Brg@users.noreply.github.com>
2023-01-09 20:42:31 -0600

    Add files via upload

---
+++ Hello_world_.java
@@ -0,0 +1,12 @@
+package hello_world_;
+
+public class Hello_world_ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!");
+		
+	}
+
+}
